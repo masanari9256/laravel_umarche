1d4c07df08e29d91570df140cbe262a48c1fe66d
michiya-lassic <lasmmich@mediba.jp>
2022-11-19 11:31:39 +0900

    classBase

--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,3 +1,6 @@
 <x-tests.app>
+    <x-slot name="header">へっだー</x-slot>
     コンポーネントテスト2
+    <x-test-class-base classBaseMessage="めっせーじ" />
+    <x-test-class-base classBaseMessage="めっせーじ" defaultMessage="へんすう" />
 </x-tests.app>
