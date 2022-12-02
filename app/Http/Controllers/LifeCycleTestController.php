c3d2d19c15a685e4bb0995e5a7e4862a221def64
lasmmich <lasmmich@mediba.jp>
2022-12-02 20:11:35 +0900

    サービスプロバイダーテスト

--- app/Http/Controllers/LifeCycleTestController.php
+++ app/Http/Controllers/LifeCycleTestController.php
@@ -6,6 +6,16 @@ use Illuminate\Http\Request;
 
 class LifeCycleTestController extends Controller
 {
+    public function serviceProviderTest()
+    {
+        $encrypt = app()->make("encrypter");
+        $password = $encrypt->encrypt("password");
+
+        $sample = app()->make("serviceProviderTest");
+
+        dd($sample, $password, $encrypt->decrypt($password));
+    }
+
     public function lifeCycleTest()
     {
         app()->bind('lifeCycleTest', function () {
